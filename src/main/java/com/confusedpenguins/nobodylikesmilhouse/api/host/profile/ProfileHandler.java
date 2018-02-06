23f7e16a6c04c44ae602f525d7f1d18a62d8fe43
arrayindex <arrayindex@gmail.com>
2018-02-06 23:40:54 +0000

    Fixing Lambda

--- src/main/java/com/confusedpenguins/nobodylikesmilhouse/api/host/profile/ProfileHandler.java
+++ src/main/java/com/confusedpenguins/nobodylikesmilhouse/api/host/profile/ProfileHandler.java
@@ -2,12 +2,14 @@ package com.confusedpenguins.nobodylikesmilhouse.api.host.profile;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.confusedpenguins.nobodylikesmilhouse.api.web.LambdaProxyResponse;
 
 import java.util.HashMap;
 
-public class ProfileHandler implements RequestHandler<ProfileRequest, ProfileResponse> {
+public class ProfileHandler implements RequestHandler<ProfileRequest, LambdaProxyResponse> {
     @Override
-    public ProfileResponse handleRequest(ProfileRequest input, Context context) {
-        return new ProfileResponse("Hello World 123");
+    public LambdaProxyResponse handleRequest(ProfileRequest input, Context context) {
+        LambdaProxyResponse lambdaProxyResponse = new LambdaProxyResponse(200, new HashMap<String, String>(), "{hello:false}");
+        return lambdaProxyResponse;
     }
 }
