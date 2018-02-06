4af8681c6f96074eaf0eacffc458231f5ccc9520
arrayindex <arrayindex@gmail.com>
2018-02-06 19:11:36 +0000

    Fixing Lambda

--- src/main/java/com/confusedpenguins/nobodylikesmilhouse/api/host/BaseHandler.java
+++
@@ -1,6 +0,0 @@
-package com.confusedpenguins.nobodylikesmilhouse.api.host;
-
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-
-public class BaseHandler implements RequestHandler<>{
-}
