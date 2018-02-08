eabb74de3be64b5f87c7578840b7a3758fa084a6
arrayindex <arrayindex@gmail.com>
2018-02-08 10:12:55 +0000

    Updated Logger

--- src/main/java/com/confusedpenguins/nobodylikesmilhouse/api/host/profile/ProfileHandler.java
+++ src/main/java/com/confusedpenguins/nobodylikesmilhouse/api/host/profile/ProfileHandler.java
@@ -9,10 +9,13 @@ import com.confusedpenguins.nobodylikesmilhouse.api.web.HttpStatus;
 import com.confusedpenguins.nobodylikesmilhouse.api.web.HttpStatusAnswer;
 import com.confusedpenguins.nobodylikesmilhouse.api.web.LambdaProxyResponse;
 import com.confusedpenguins.nobodylikesmilhouse.api.web.answers.UnauthorizedAnswer;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import java.util.HashMap;
 
 public class ProfileHandler implements RequestHandler<ProfileRequest, LambdaProxyResponse> {
+    private final Logger logger = LogManager.getLogger(ProfileHandler.class);
     UseCaseFactory factory;
 
     public ProfileHandler() {
@@ -20,17 +23,21 @@ public class ProfileHandler implements RequestHandler<ProfileRequest, LambdaProx
         ProductionDirector director = new ProductionDirector();
         director.construct();
         factory = director.getUseCaseFactory();
+
+        logger.info("Constructor Kicked");
     }
 
     @Override
     public LambdaProxyResponse handleRequest(ProfileRequest input, Context context) {
-
+        logger.info("handleRequest Start");
         Identity identity = this.factory.newAuthenticateUserUseCase(input.getFacebookToken()).execute();
 
-        if(identity == null) {
+        if (identity == null) {
+            logger.info("Identity Null");
             return new UnauthorizedAnswer();
         }
 
+        logger.info("We Gotta Thing");
         LambdaProxyResponse lambdaProxyResponse = new LambdaProxyResponse(HttpStatus.SUCCESS, new ProfileResponse(identity));
         return lambdaProxyResponse;
     }
